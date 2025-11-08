18976c804ea84d90dbcb89a76e15f89a517b1d43
Lancasterg <lancaster0180@gmail.com>
2025-11-08 21:22:16 +0000

    Write descriptive comments for public api

--- pysafe_config/__init__.py
+++ pysafe_config/__init__.py
@@ -17,7 +17,13 @@ def getenv_bool(
     var_name: str, default: bool | None = None, required: bool = True
 ) -> bool | None:
     """
-    Get the value of an environment variable specified by `var_name`, returned as a boolean.
+   Get the value of a bool environment variable `var_name`
+
+   If the environment variable is not set:
+       - If `required` is True, a RuntimeError is raised indicating that the variable
+         is mandatory.
+       - If `required` is False, the function returns the default value, which may be
+         None if no default is provided.
 
     Valid boolean strings are case-insensitive.
     Acceptable values are:
@@ -32,12 +38,6 @@ def getenv_bool(
         | "enable"     | "disable"    |
         | "enabled"    | "disabled"   |
         | "t"          | "f"          |
-
-    If the environment variable is not set:
-      - If `required` is True, a RuntimeError is raised indicating that the variable
-        is mandatory.
-      - If `required` is False, the function returns the default value, which may be
-        None if no default is provided.
 
     Args:
         var_name (str): The name of the environment variable to retrieve.
@@ -63,13 +63,30 @@ def getenv_float(
     var_name: str, default: float | None = None, required: bool = True
 ) -> float | None:
     """
-    Get the value of an environment variable specified by `var_name`, returned as a float.
+   Get the value of a float environment variable `var_name`
 
     If the environment variable is not set:
-      - If `required` is True, a RuntimeError is raised indicating that the variable
-        is mandatory.
-      - If `required` is False, the function returns the default value, which may be
-        None if no default is provided.
+        - If `required` is True, a RuntimeError is raised indicating that the variable
+          is mandatory.
+        - If `required` is False, the function returns the default value, which may be
+          None if no default is provided.
+
+    Valid float strings must:
+        - Contain only digits (`0-9`), optionally preceded by a single `+` or `-` sign
+        - Include exactly one decimal point to separate the whole and fractional parts
+        - Not contain any whitespace, commas, or alphabetic characters
+
+    Examples:
+
+        | Valid strings | Invalid strings |
+        |----------------|-----------------|
+        | "50.2"         | "50"            |
+        | "-0.0"         | "5.5.5"         |
+        | "+1000.5"      | " 12.3"         |
+        | "-99.0"        | "12,3"          |
+        | "0.0001"       | "ten"           |
+        | "+.5"          | "5."            |
+        | "-1.23"        | "" (empty)      |
 
     Args:
         var_name (str): The name of the environment variable to retrieve.
@@ -95,13 +112,13 @@ def getenv_str(
     var_name: str, default: str | None = None, required: bool = True
 ) -> str | None:
     """
-    Get the value of an environment variable specified by `var_name`, returned as a string.
+   Get the value of a str environment variable `var_name`
 
     If the environment variable is not set:
-      - If `required` is True, a RuntimeError is raised indicating that the variable
-        is mandatory.
-      - If required is False, the function returns the default value, which may be
-        None if no default is provided.
+        - If `required` is True, a RuntimeError is raised indicating that the variable
+          is mandatory.
+        - If required is False, the function returns the default value, which may be
+          None if no default is provided.
 
     Args:
         var_name (str): The name of the environment variable to retrieve.
@@ -127,13 +144,31 @@ def getenv_int(
     var_name: str, default: int | None = None, required: bool = True
 ) -> int | None:
     """
-    Get the value of an environment variable specified by `var_name`, returned as an integer.
+   Get the value of an int environment variable `var_name`
 
     If the environment variable is not set:
-      - If `required` is True, a RuntimeError is raised indicating that the variable
-        is mandatory.
-      - If `required` is False, the function returns the default value, which may be
-        None if no default is provided.
+        - If `required` is True, a RuntimeError is raised indicating that the variable
+          is mandatory.
+        - If `required` is False, the function returns the default value, which may be
+          None if no default is provided.
+
+    Valid integer environment variables must:
+        - Contain only digits (`0-9`), optionally preceded by a single `+` or `-` sign
+        - Not contain any whitespace
+        - Not include decimal points, letters, or special symbols
+
+    Examples:
+
+        | Valid strings | Invalid strings |
+        |----------------|-----------------|
+        | "100"          | " 100"          |
+        | "1"            | "10.5"          |
+        | "-50"          | "1,000"         |
+        | "+1000"        | "12a"           |
+        | "0"            | "++5"           |
+        | "-0"           | "5-"            |
+        | "123456        | "ten"           |
+        | "-123456"      | "" (empty)      |
 
     Args:
         var_name (str): The name of the environment variable to retrieve.
@@ -158,7 +193,22 @@ def getenv_int(
 
 def getenv_bool_strict(var_name: str) -> bool:
     """
-    Get the value of an environment variable, guaranteeing the return type as a boolean.
+    Get the value of a bool environment variable `var_name`
+    raising a RuntimeError If the environment variable is not set.
+
+    Valid boolean strings are case-insensitive.
+    Acceptable values are:
+
+        | True values | False values |
+        |--------------|--------------|
+        | "true"       | "false"      |
+        | "1"          | "0"          |
+        | "yes"        | "no"         |
+        | "y"          | "n"          |
+        | "on"         | "off"        |
+        | "enable"     | "disable"    |
+        | "enabled"    | "disabled"   |
+        | "t"          | "f"          |
 
     Args:
         var_name (str): The name of the environment variable to retrieve.
@@ -177,15 +227,16 @@ def getenv_bool_strict(var_name: str) -> bool:
 
 def getenv_float_strict(var_name: str) -> float:
     """
-    Get the value of an environment variable, guaranteeing the return type as a float.
+    Get the value of a float environment variable `var_name`
+    raising a RuntimeError If the environment variable is not set.
 
     The value must represent a valid floating-point number.
     A single decimal point is required in the variable value; otherwise, a `TypeError` will be raised.
 
     Valid float strings must:
-      - Contain only digits (`0-9`), optionally preceded by a single `+` or `-` sign
-      - Include exactly one decimal point to separate the whole and fractional parts
-      - Not contain any whitespace, commas, or alphabetic characters
+        - Contain only digits (`0-9`), optionally preceded by a single `+` or `-` sign
+        - Include exactly one decimal point to separate the whole and fractional parts
+        - Not contain any whitespace, commas, or alphabetic characters
 
     Examples:
 
@@ -214,33 +265,15 @@ def getenv_float_strict(var_name: str) -> float:
     return _getenv_strict(var_name, float, _str_to_float)
 
 
-def getenv_str_strict(var_name: str) -> str:
-    """
-    Get the value of an environment variable, guaranteeing the return type as a string.
-
-    Args:
-        var_name (str): The name of the environment variable to retrieve.
-
-    Returns:
-        str: The string value of the environment variable.
-
-    Raises:
-        TypeError: If the environment variable is set but cannot be converted to a string.
-        RuntimeError: If the environment variable is not set.
-    """
-    from ._helper_str import _str_to_str
-
-    return _getenv_strict(var_name, str, _str_to_str)
-
-
 def getenv_int_strict(var_name: str) -> int:
     """
-    Get the value of an environment variable, guaranteeing the return type as an integer.
+    Get the value of an int environment variable `var_name`
+    raising a RuntimeError If the environment variable is not set.
 
-    Valid integer strings must:
-      - Contain only digits (`0-9`), optionally preceded by a single `+` or `-` sign
-      - Not contain any whitespace
-      - Not include decimal points, letters, or special symbols
+    Valid integer environment variables must:
+        - Contain only digits (`0-9`), optionally preceded by a single `+` or `-` sign
+        - Not contain any whitespace
+        - Not include decimal points, letters, or special symbols
 
     Examples:
 
@@ -252,8 +285,8 @@ def getenv_int_strict(var_name: str) -> int:
         | "+1000"        | "12a"           |
         | "0"            | "++5"           |
         | "-0"           | "5-"            |
-        | "123456         | "ten"           |
-        | "-123456"       | "" (empty)      |
+        | "123456        | "ten"           |
+        | "-123456"      | "" (empty)      |
 
     Args:
         var_name (str): The name of the environment variable to retrieve.
@@ -269,3 +302,22 @@ def getenv_int_strict(var_name: str) -> int:
 
     return _getenv_strict(var_name, int, _str_to_int)
 
+
+def getenv_str_strict(var_name: str) -> str:
+    """
+    Get the value of a str environment variable `var_name`
+    raising a RuntimeError If the environment variable is not set.
+
+    Args:
+        var_name (str): The name of the environment variable to retrieve.
+
+    Returns:
+        str: The string value of the environment variable.
+
+    Raises:
+        TypeError: If the environment variable is set but cannot be converted to a string.
+        RuntimeError: If the environment variable is not set.
+    """
+    from ._helper_str import _str_to_str
+
+    return _getenv_strict(var_name, str, _str_to_str)
